0aaec12736de8f075c4b1caea34a119313bdb758
Ty Mason <hi@tygr.dev>
2024-01-23 23:52:59 +0000

    chore: fix cors config

--- next.config.mjs
+++ next.config.mjs
@@ -9,19 +9,58 @@ const nextConfig = {
             { hostname: "db.katarogu.tygr.dev" }
         ]
     },
-    async headers() {
+    headers() {
         return [
             {
-                source: "https://db.katarogu.tygr.dev",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://db.katarogu.tygr.dev" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
+                source: '/(.*)',
+                headers: securityHeaders,
+            },
+        ];
+    },
 };
+
+// https://nextjs.org/docs/advanced-features/security-headers
+const ContentSecurityPolicy = `
+    default-src 'self' vercel.live analytics.tygr.dev db.katarogu.tygr.dev;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' vercel.live analytics.tygr.dev db.katarogu.tygr.dev;
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    media-src 'none';
+    connect-src *;
+    font-src 'self';
+`;
+
+const securityHeaders = [
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+    {
+        key: 'Content-Security-Policy',
+        value: ContentSecurityPolicy.replace(/\n/g, ''),
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+    {
+        key: 'Referrer-Policy',
+        value: 'origin-when-cross-origin',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+    {
+        key: 'X-Frame-Options',
+        value: 'DENY',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=31536000; includeSubDomains; preload',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+    {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=()',
+    },
+];
 
 export default nextConfig;
