a814402f8fdde84051086966e9e3e79b4e0ea47f
Ty Mason <hi@tygr.dev>
2024-02-10 19:19:20 +0000

    fix: remove my tailscale ip lmao

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
     images: {
         remotePatterns: [
             { hostname: "127.0.0.1" },
-            { hostname: "100.84.155.75" },
             { hostname: "localhost" },
             { hostname: "api.dicebear.com" },
             { hostname: "images.unsplash.com" },
