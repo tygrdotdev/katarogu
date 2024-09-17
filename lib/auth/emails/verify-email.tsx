4c33aa6d5c459ccec9f7a4d546266a3453b3f613
Ty Mason <hi@tygr.dev>
2024-09-17 15:17:45 +0100

    feat: update verify account email

--- lib/auth/emails/verify-email.tsx
+++ lib/auth/emails/verify-email.tsx
@@ -1,141 +1,134 @@
 import {
 	Body,
+	Button,
 	Container,
 	Head,
 	Heading,
 	Hr,
 	Html,
 	Img,
+	Link,
 	Preview,
 	Section,
 	Text,
 } from "@react-email/components";
 import * as React from "react";
 
-const baseUrl = process.env.VERCEL_URL
-	? `https://${process.env.VERCEL_URL}`
-	: `http://localhost:3000`;
-
-export default function VerifyEmail({
-	verificationCode = "596853",
-}: { verificationCode?: string }) {
-	return (
-		<Html>
-			<Head />
-			<Preview>Katarogu Email Verification</Preview>
-			<Body style={main}>
-				<Container>
-					<Section style={coverSection}>
-						<Section style={imageSection}>
-							<Img
-								src={`${baseUrl}/email/logo.png`}
-								width="75"
-								height="75"
-								alt="Katarogu Logo"
-							/>
-						</Section>
-						<Section style={upperSection}>
-							<Heading style={h1}>Almost there!</Heading>
-							<Text style={mainText}>
-								Thank you for signing up to Katarogu. To complete
-								your registration, please verify your email address
-								by entering the following code:
-							</Text>
-							<Section style={verificationSection}>
-								<Text style={verifyText}>Verification code</Text>
-
-								<Text style={codeText}>{verificationCode}</Text>
-								<Text style={validityText}>
-									(This code is valid for 15 minutes)
-								</Text>
-							</Section>
-						</Section>
-						<Hr />
-						<Section style={lowerSection}>
-							<Text style={cautionText}>
-								Katarogu will never email you and ask you to disclose or verify your password.
-								<br />
-								If you did not sign up for an account, you can safely ignore this email.
-							</Text>
-						</Section>
-					</Section>
-				</Container>
-			</Body>
-		</Html>
-	);
+interface VerifyAccountEmailProps {
+	verificationCode: string;
 }
 
+const baseUrl = process.env.VERCEL_URL
+	? `https://${process.env.VERCEL_URL}`
+	: "https://beta.katarogu.tygr.dev";
+
+export const VerifyAccountEmail = ({
+	verificationCode,
+}: VerifyAccountEmailProps) => (
+	<Html>
+		<Head />
+		<Preview>Welcome to Katarogu!</Preview>
+		<Body style={main}>
+			<Container style={container}>
+				<Img
+					src={`${baseUrl}/email/logo.png`}
+					width="42"
+					height="42"
+					alt="Katarogu"
+					style={logo}
+				/>
+				<Heading style={heading}>Welcome to Katarogu!</Heading>
+				<Section style={buttonContainer}>
+					<Button style={button} href={`${baseUrl}/auth/verify?code=${verificationCode}`}>
+						Verify Account
+					</Button>
+				</Section>
+				<Text style={paragraph}>
+					This link and code will only be valid for the next 15 minutes. If the
+					link does not work, you can use the login verification code directly:
+				</Text>
+				<code style={code}>{verificationCode}</code>
+				<Hr style={hr} />
+				<Link href="https://tygr.dev" style={reportLink}>
+					By @tygrdev
+				</Link>
+			</Container>
+		</Body>
+	</Html>
+);
+
+VerifyAccountEmail.PreviewProps = {
+	verificationCode: "123456",
+} as VerifyAccountEmailProps;
+
+export default VerifyAccountEmail;
+
+const logo = {
+	width: 42,
+	height: 42,
+};
+
 const main = {
-	backgroundColor: "#fff",
-	color: "#212121",
+	backgroundColor: "#ffffff",
+	fontFamily:
+		'-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
 };
 
-const h1 = {
-	color: "#333",
-	fontFamily:
-		"-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-	fontSize: "20px",
-	fontWeight: "bold",
-	marginBottom: "15px",
+const container = {
+	margin: "0 auto",
+	padding: "20px 0 48px",
+	maxWidth: "560px",
 };
 
-// const link = {
-// 	color: "#2754C5",
-// 	fontFamily:
-// 		"-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-// 	fontSize: "14px",
-// 	textDecoration: "underline",
-// };
+const heading = {
+	fontSize: "24px",
+	letterSpacing: "-0.5px",
+	lineHeight: "1.3",
+	fontWeight: "400",
+	color: "#484848",
+	padding: "17px 0 0",
+};
 
-const text = {
-	color: "#333",
-	fontFamily:
-		"-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+const paragraph = {
+	margin: "0 0 15px",
+	fontSize: "15px",
+	lineHeight: "1.4",
+	color: "#3c4149",
+};
+
+const buttonContainer = {
+	padding: "27px 0 27px",
+};
+
+const button = {
+	backgroundColor: "#121212",
+	borderRadius: "3px",
+	fontWeight: "600",
+	color: "#fff",
+	fontSize: "15px",
+	textDecoration: "none",
+	textAlign: "center" as const,
+	display: "block",
+	padding: "11px 23px",
+};
+
+const reportLink = {
 	fontSize: "14px",
-	margin: "24px 0",
+	color: "#b4becc",
 };
 
-const imageSection = {
-	backgroundColor: "#111",
-	display: "flex",
-	padding: "20px 0",
-	alignItems: "center",
-	justifyContent: "center",
+const hr = {
+	borderColor: "#dfe1e4",
+	margin: "42px 0 26px",
 };
 
-const coverSection = { backgroundColor: "#fff" };
-
-const upperSection = { padding: "25px 35px" };
-
-const lowerSection = { padding: "25px 35px" };
-
-const verifyText = {
-	...text,
-	margin: 0,
-	fontWeight: "bold",
-	textAlign: "center" as const,
+const code = {
+	fontFamily: "monospace",
+	fontWeight: "700",
+	padding: "1px 4px",
+	backgroundColor: "#dfe1e4",
+	letterSpacing: "-0.3px",
+	fontSize: "21px",
+	borderRadius: "4px",
+	color: "#3c4149",
 };
-
-const codeText = {
-	...text,
-	fontWeight: "bold",
-	fontSize: "36px",
-	margin: "10px 0",
-	textAlign: "center" as const,
-};
-
-const validityText = {
-	...text,
-	margin: "0px",
-	textAlign: "center" as const,
-};
-
-const verificationSection = {
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
-};
-
-const mainText = { ...text, marginBottom: "14px" };
-
-const cautionText = { ...text, margin: "0px" };
