ecc97fda3fd29e08958a4f3e3fc62461503acb1c
Ty Mason <hi@tygr.dev>
2024-10-28 19:46:10 +0000

    fix: prevent safari from zooming on input select

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
